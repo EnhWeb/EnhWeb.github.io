5464695c8b1e8364ed8a18419f6c2f39b88cb2b1
闵文辉-笔记本 <101060493@qq.com>
2017-09-18 23:40:38 +0800

    update

---
+++ Content/EnhWebApp/controllers/EnhWebApp.myController3.js
@@ -0,0 +1,7 @@
+﻿"use strict";
+
+EnhWebApp.controller("myController3", function ($scope, $http, baseUrl) {
+    //$scope.IndestyResource = $resource(baseUrl + ":id", { id: "@id" });
+    //console.log("baseUrl", baseUrl);
+    //console.log("$scope.IndestyResource", $scope.IndestyResource);
+});
