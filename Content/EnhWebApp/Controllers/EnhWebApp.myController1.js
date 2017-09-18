b56b8c23e63697b5cb75f61e5965c0fa887a3e20
闵文辉-笔记本 <101060493@qq.com>
2017-09-19 00:23:52 +0800

    update

--- Content/EnhWebApp/Controllers/EnhWebApp.myController1.js
+++ Content/EnhWebApp/Controllers/EnhWebApp.myController1.js
@@ -2,4 +2,12 @@
 
 EnhWebApp.controller("myController1", function ($scope, $http, baseUrl) {
     //console.log("baseUrl：", baseUrl);
+    $scope.Industrys = [];
+    $scope.alert = function (msg) {
+        window.alert(msg);
+    };
+
+    $http.get(baseUrl + "TestBaseInfo/GetIndustrys", {}).success(function (data, textStatus, jqXHR) {
+        $scope.Industrys = data;
+    });
 });
